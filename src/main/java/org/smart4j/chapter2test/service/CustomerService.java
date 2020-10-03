2f647a8231c308adbaed654affd1849d616830ce
fucker0ZEO <57436128+fucker0ZEO@users.noreply.github.com>
2020-10-03 22:35:12 +0800

    1day第2次提交

--- src/main/java/org/smart4j/chapter2test/service/CustomerService.java
+++ src/main/java/org/smart4j/chapter2test/service/CustomerService.java
@@ -1,9 +1,11 @@
 package org.smart4j.chapter2test.service;
 
 import org.smart4j.chapter2test.model.Customer;
+import org.smart4j.chapter2test.util.PropsUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class CustomerService {
     /**
@@ -48,5 +50,24 @@ public class CustomerService {
      public List<Customer> getCustomerList() {
      }
      */
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties conf= PropsUtil.loadProps("config.properties");
+        DRIVER=conf.getProperty("jdbc.driver");
+        URL=conf.getProperty("jdbc.url");
+        USERNAME=conf.getProperty("jdbc.username");
+        PASSWORD=conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e){
+           // LOGGER.error("can not load jdbc driver",e);
+            //Logger.ERROR("can not load jdbc driver",e);
+        }
+    }
 
 }
--- src/main/java/org/smart4j/chapter2test/util/PropsUtil.java
+++ src/main/java/org/smart4j/chapter2test/util/PropsUtil.java
@@ -1,19 +1,28 @@
 package org.smart4j.chapter2test.util;
 
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
-import javax.imageio.IIOException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.logging.Logger;
+
+/**import java.util.logging.Logger;
+ * 这里存在着自动导包不注意时的错
+ * 本来应该 import org.slf4j.Logger;
+ * 结果却导了 java.util.logging.Logger
+ * 导致后面做了一个类型转换，甚至是无法在其他类中调用LOGGER
+ * 更无法输出error级别的日志，之前了解过JDK支持的输入日志和log4j是不同的，
+ * 其中的差异之一就是error日志，那个时候就怀疑调用的是JDK自带的日志，而不是slf4j
+ */
 
 public final class PropsUtil {
     /**
-     * 这里和原书略有不同，做了一个类型转换 (Logger)
+     * 这里和原书略有不同，做了一个类型转换 (Logger)--这是导包出错导致的
      */
-    private static final Logger LOGGER= (Logger) LoggerFactory.getLogger(PropsUtil.class);
+    private static final Logger LOGGER=  LoggerFactory.getLogger(PropsUtil.class);
+
 
     /**
      * 加载属性文件
@@ -22,30 +31,21 @@ public final class PropsUtil {
         Properties props=null;
         InputStream is=null;
         try {
-            is=Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
-            if(is == null){
-                try {
-                    throw new FileNotFoundException(fileName+"file is not found");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new FileNotFoundException(fileName + "file is not found");
             }
-            props=new Properties();
-            try {
-                props.load(is);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (IIOException e){
+            props = new Properties();
+            props.load(is);
+        }catch (IOException e){
             LOGGER.error("load properties file failure",e);
-            
-        }finally {
+        } finally {
             if(is != null){
                 try {
                     is.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    //e.printStackTrace();
+                    LOGGER.error("close input stream failure",e);
                 }
             }
         }
@@ -59,7 +59,7 @@ public final class PropsUtil {
      * 而实际实现时根据IDE提示删除了冗余参数来调用
      */
     public static String getString(Properties props,String key){
-        return getString(props, key);
+        return getString(props, key,"");
     }
     /**
      * 获取字符串属性（可指定默认值）
@@ -68,6 +68,38 @@ public final class PropsUtil {
         String value=defaultValue;
         if(props.containsKey(key)){
             value=props.getProperty(key);
+        }
+        return value;
+    }
+    /**
+     * 获取数值型属性（默认值为0）
+     */
+    public static int getInt(Properties props,String key){
+        return getInt(props, key,0);
+    }
+    /**
+     * 获取数值型属性（可指定默认值）
+     */
+    public static int getInt(Properties props,String  key,int defaultValue){
+        int value=defaultValue;
+        if(props.containsKey(key)){
+            value=CastUtil.castInt(props.getProperty(key));
+        }
+        return value;
+    }
+    /**
+     * 获取boolean型属性（默认值为false）
+     */
+    public static boolean getBoolean(Properties props,String key){
+        return getBoolean(props, key,false);
+    }
+    /**
+     * 获取boolean型属性（可指定默认值）
+     */
+    public static boolean getBoolean(Properties pros,String key,Boolean defaultValue){
+        boolean value=defaultValue;
+        if(pros.containsKey(key)){
+            value=CastUtil.castBoolean(pros.getProperty(key));
         }
         return value;
     }
